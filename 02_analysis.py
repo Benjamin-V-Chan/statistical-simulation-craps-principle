49ea2262a45bcdca01124a22b273e9e22196a1b6
Benjamin Chan <benjamin.v.chan@gmail.com>
2025-02-08 23:57:26 -0800

    02 script outline

---
+++ 02_analysis.py
@@ -0,0 +1,8 @@
+# Import necessary modules (pandas, numpy)
+# Load simulation data from the CSV file
+# Compute key statistics:
+#    - Probability of winning
+#    - Probability of losing
+#    - Expected return per game
+#    - Variance and standard deviation
+# Save statistical results to a JSON file in outputs/
